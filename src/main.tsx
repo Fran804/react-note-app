8fa84755d8aef985580a6bda7f59ed9a4b6ba7aa
Fran804 <franciscovillalobos804@gmail.com>
2023-07-17 19:18:49 -0600

    Replace browserRouter for hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import {ThemeContextProvider} from "./context/ThemeContext.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeContextProvider>
 );
